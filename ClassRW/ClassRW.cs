26071515ef3c22c61e832f391d45281e4b0c4928
Oscar Davies <oscar.william.davies@outlook.com>
2019-09-22 17:41:00 +0100

    Read and Write Classes to a file

---
+++ ClassRW/ClassRW.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace ClassRW
+{
+    public static class ClassReadWriter
+    {
+        public static void WriteObject(object SrcObject, Type ObjectType, StreamWriter Writer, int IndentationDepth = 0)
+        {
+            foreach (FieldInfo Field in ObjectType.GetFields())
+            {
+                object FieldValue = Field.GetValue(SrcObject);
+                if (FieldValue == null) { WriteItem(Field.Name + ":NULL", Writer, IndentationDepth); }
+                else if (Field.FieldType.IsSerializable) { WriteItem(Field.Name + ":" + FieldValue, Writer, IndentationDepth); }
+                else
+                {
+                    WriteItem(Field.Name + ":{", Writer, IndentationDepth);
+                    WriteObject(FieldValue, Field.FieldType, Writer, IndentationDepth + 1);
+                    WriteItem("}", Writer, IndentationDepth);
+                }
+            }
+        }
+
+        static void WriteItem(string Data, StreamWriter Writer, int IndentationDepth=0)
+        {
+            Writer.WriteLine(new string(' ', IndentationDepth*3) + Data);
+        }
+
+        public static object ReadObject(Type ObjectType, StreamReader Reader)
+        {
+            object ReturnObject = Activator.CreateInstance(ObjectType,null);
+            while (!Reader.EndOfStream)
+            {
+                string Line = Reader.ReadLine().TrimStart(" ".ToCharArray()); string[] LineArr = Line.Split(new string[] { ":" }, StringSplitOptions.None);
+                if (!Line.Contains("}"))
+                {
+                    FieldInfo thisField = ObjectType.GetField(LineArr[0]);
+                    if (LineArr[1] == "NULL") { thisField.SetValue(ReturnObject, null); }
+                    else if (LineArr[1].StartsWith("{")) { thisField.SetValue(ReturnObject,ReadObject(thisField.FieldType, Reader)); }
+                    else  { thisField.SetValue(ReturnObject, Convert.ChangeType(LineArr[1], thisField.FieldType)); }
+                }
+                else { break; }
+            }
+            return ReturnObject;
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ClassRW;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestObject TO = new TestObject(); TO.T0 = new TestObject(); TO.T0.T0 = new TestObject();
+            StreamWriter Writer = new StreamWriter("./Out.txt", false);
+            ClassReadWriter.WriteObject(TO,TO.GetType(),Writer);
+            Writer.Flush(); Writer.Close();
+
+            StreamReader Reader = new StreamReader("./Out.txt");
+            var v = ClassReadWriter.ReadObject(TO.GetType(), Reader);
+        }
+    }
+
+    public class TestObject
+    {
+        public string S = "yee haw";
+        public int I = 9;
+        public TestObject T0;
+        public bool B = true;
+    }
+
+}
